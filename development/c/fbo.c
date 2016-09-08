877dd75ac75ee73561da761924c3256e9a4b96b3
983 <99991@users.noreply.github.com>
2016-09-08 11:45:46 +0200

    removed some files and changed nvidia benchmark to only create one fbo

--- development/c/fbo.c
+++ development/c/fbo.c
@@ -11,13 +11,6 @@
 
 #define CHECK check_error(__LINE__);
 
-/* slower, but smoother */
-#if 1
-#define DRAW_SMOOTH
-#endif
-
-GLuint path;
-
 GLuint fbo;
 GLuint texture;
 GLuint stencil_texture;
@@ -25,43 +18,12 @@ GLuint timeElapsedQuery;
 
 int width = 64;
 int height = 64;
-int fbo_width = 4096;
-int fbo_height = 4096;
 
 void check_error(int line){
     GLenum error = glGetError();
     if (error != GL_NO_ERROR){
         printf("ERROR in line %d: %s\n", line, (const char*)gluErrorString(error));
     }
-}
-
-char* load_text(const char *path, size_t *n_bytes){
-    FILE *fp = fopen(path, "rb");
-    assert(fp);
-    fseek(fp, 0, SEEK_END);
-    size_t n = ftell(fp);
-    rewind(fp);
-    char *data = malloc(n + 1);
-    fread(data, 1, n, fp);
-    data[n] = '\0';
-    if (n_bytes) *n_bytes = n;
-    fclose(fp);
-    return data;
-}
-
-void init_nvpr(const char *file_path){
-    CHECK
-    glClearStencil(0);
-    CHECK
-    glStencilFunc(GL_NOTEQUAL, 0, 0x1F);
-    glStencilOp(GL_KEEP, GL_KEEP, GL_ZERO);
-    CHECK
-    path = glGenPathsNV(1);
-    size_t n;
-    char *path_src = load_text(file_path, &n);
-    glPathStringNV(path, GL_PATH_FORMAT_SVG_NV, (GLsizei)n, path_src);
-    free(path_src);
-    CHECK
 }
 
 void make_texture(void){
@@ -72,14 +34,14 @@ void make_texture(void){
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-#ifdef DRAW_SMOOTH
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-#else
+#if 1
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+#else
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 #endif
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, fbo_width, fbo_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 }
 
 void make_stencil_texture(void){
@@ -93,7 +55,7 @@ void make_stencil_texture(void){
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, fbo_width, fbo_height, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, width, height, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);
 }
 
 void make_fbo(void){
@@ -104,7 +66,7 @@ void make_fbo(void){
 
     assert(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE);
 }
-
+/*
 void render_to_fbo(void){
     glBindFramebuffer(GL_FRAMEBUFFER, fbo);
     glViewport(0, 0, width, height);
@@ -148,7 +110,7 @@ void render_from_fbo_to_screen(void){
     glBindTexture(GL_TEXTURE_2D, texture);
     glColor3f(1.0f, 1.0f, 1.0f);
 
-#ifdef DRAW_SMOOTH
+#if 0
     glGenerateMipmap(GL_TEXTURE_2D);
 #endif
 
@@ -161,27 +123,62 @@ void render_from_fbo_to_screen(void){
 
     glDisable(GL_TEXTURE_2D);
 }
-
+*/
 void cleanup(void){
     glDeleteTextures(1, &texture);
     glDeleteTextures(1, &stencil_texture);
     glDeleteFramebuffers(1, &fbo);
 }
 
+void make_rect(float *pos, float x0, float y0, float x1, float y1){
+    *pos++ = x0; *pos++ = y0;
+    *pos++ = x1; *pos++ = y0;
+    *pos++ = x1; *pos++ = y1;
+
+    *pos++ = x0; *pos++ = y0;
+    *pos++ = x1; *pos++ = y1;
+    *pos++ = x0; *pos++ = y1;
+}
+
+void foo(void){
+    glEnable(GL_STENCIL_TEST);
+    glClearStencil(0);
+
+    /* prepare writing to stencil buffer */
+    glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+    glStencilFunc(GL_NEVER, 0, 1);
+    glStencilOp(GL_INVERT, GL_INVERT, GL_INVERT);
+
+    float pos[4*3];
+    make_rect(pos + 0*3, 0.0f, 0.0f, 0.8f, 0.8f);
+    make_rect(pos + 2*3, 0.1f, 0.1f, 0.7f, 0.7f);
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(2, GL_FLOAT, 0, pos);
+    glDrawArrays(GL_TRIANGLES, 0, 4*3);
+
+    /* prepare coloring stencil buffer */
+    glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+    glStencilFunc(GL_EQUAL, 1, 1);
+    glStencilOp(GL_ZERO, GL_ZERO, GL_ZERO);
+
+    glDrawArrays(GL_TRIANGLES, 0, 2*3);
+
+    glDisable(GL_STENCIL_TEST);
+}
+
 void display(void){
-    static int pixels_increment = 256*256;
-    static int pixels = 0;
-    pixels += pixels_increment;
+    make_texture();
+    make_stencil_texture();
+    make_fbo();
+    //render_to_fbo();
+    //render_from_fbo_to_screen();
 
-    if (pixels > 4096*4096){
-        exit(0);
-    }
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-    width = sqrt(pixels);
-    height = sqrt(pixels);
+    foo();
 
-    render_to_fbo();
-    render_from_fbo_to_screen();
     glutSwapBuffers();
 
     uint64_t t;
@@ -202,11 +199,6 @@ int main(int argc, char **argv) {
     glutInitWindowSize(800, 800);
     glutInit(&argc, argv);
     glutCreateWindow("Benchmark");
-#if 0
-    printf("vendor: %s\n", glGetString(GL_VENDOR));
-    printf("version: %s\n", glGetString(GL_VERSION));
-    printf("renderer: %s\n", glGetString(GL_RENDERER));
-#endif
     glutDisplayFunc(display);
 
     GLenum status = glewInit();
@@ -214,27 +206,8 @@ int main(int argc, char **argv) {
         printf("OpenGL Extension Wrangler (GLEW) failed to initialize");
         exit(-1);
     }
-    GLboolean hasDSA = glewIsSupported("GL_EXT_direct_state_access");
-    if (!hasDSA) {
-        printf("OpenGL implementation doesn't support GL_EXT_direct_state_access (you should be using NVIDIA GPUs...)");
-        exit(-1);
-    }
+
     CHECK
-
-    const char *path = "../../development/py/path.txt";
-
-    if (argc == 2){
-        path = argv[1];
-    }else{
-        printf("choosing default path\n");
-    }
-
-    init_nvpr(path);
-
-    make_texture();
-    make_stencil_texture();
-    make_fbo();
-
     work(0);
     glutMainLoop();
     return 0;
---
+++ run_benchmark_nvpr.bat
@@ -0,0 +1,2 @@
+"benchmark/nvpr/nvpr_path.exe" "development/py/path.txt" > "development/figures/results_nvpr.txt"
+"benchmark/nvpr/nvpr_path.exe" "development/py/path2.txt" > "development/figures/results_nvpr2.txt"
